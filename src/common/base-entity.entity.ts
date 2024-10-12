25d9e19351ae2437ef637b5d68a739a534b8d154
Thiago Lemos <thiagolemos1995@hotmail.com>
2024-10-12 16:07:28 -0300

    feat: create User entities

---
+++ src/common/base-entity.entity.ts
@@ -0,0 +1,25 @@
+import {
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+export abstract class BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @CreateDateColumn({
+    name: 'created_at',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    nullable: true,
+  })
+  updatedAt?: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at', nullable: true })
+  deletedAt?: Date;
+}
---
+++ src/users/enums/role.enum.ts
@@ -0,0 +1,5 @@
+export enum ERole {
+  ANALYST = 'analyst',
+  REVIEWER = 'reviewer',
+  APPROVER = 'approver',
+}
